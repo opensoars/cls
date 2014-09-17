10b730f767d4de43b1edf820bbf9d2e36d643acb
opensoars <sam_1700@hotmail.com>
2014-09-17 12:50:24 +0200

    100% coverage and mocha test

--- test/coverage.js
+++ test/coverage.js
@@ -1,10 +1,59 @@
 var cls = require('../index.js');
 
-
+/** Test case #1
+ * Normal usage
+ */
 console.log(
 	cls({
 		t: '[prefix]',
 		c: 'magenta',
 		s: 'bold'
+	})
+);
+
+
+/** Test case #2
+ * Give cls nothing to work with
+ */
+console.log(
+	cls()
+);
+
+
+/** Test case #3
+ * Give wrong color type
+ */
+console.log(
+	cls({
+		t: 'Hello',
+		c: 'wrongColor'
+	})
+);
+
+
+/** Test case #4
+ * Give a style array
+ */
+console.log(
+	cls({
+		s: ['bold', 'underline']
+	})
+);
+
+/** Test case #4
+ * Give a wrong style array
+ */
+console.log(
+	cls({
+		s: ['wrong', 'style']
+	})
+);
+
+/** Test case #5
+ * Give a wrong style string
+ */
+console.log(
+	cls({
+		s: 'wrongStyle'
 	})
 );
--- test/mocha.js
+++ test/mocha.js
@@ -3,11 +3,18 @@ var assert = require('assert');
 var cls = require('../index.js');
 
 
-
 describe('cls', function (){
 
-	it('Should have more mocha tests', function (){
-		assert.equal(0, 0);
+	it('should return a string', function (){
+		assert.equal('string', typeof cls({t: 'Hello Mocha'}));
 	});
-	
+
+	it('should return an empty string when no options are given', function (){
+		assert.equal(0, cls().length);
+	});
+
+	it('should not fail when nothing is supplied', function (){
+		assert.doesNotThrow(cls);
+	});
+
 });
